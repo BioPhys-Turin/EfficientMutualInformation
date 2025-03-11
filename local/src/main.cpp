b25df745881989376b4f27dabf01a8bbf2492fc5
LeonardoAgasso <leonardo.agasso@edu.unito.it>
2025-03-11 16:07:15 +0100

    TENTATIVE: normalized mutual information

--- local/src/main.cpp
+++ local/src/main.cpp
@@ -9,21 +9,16 @@
 #include <algorithm>
 #include <omp.h>
 
+
 using namespace std;
 typedef uint8_t datatype;
 // #define DEBUG
 
 float Hxy(std::vector<datatype> &y_true, std::vector<datatype> &y_pred)
 {
-    //std::map<std::pair<datatype, datatype>, float> pxy;		// define map pxy that associate a pair of datatype with a float (joint probability)
-    //std::map<datatype, float> px;							// define map px that associate a datatype with a float (marginal probability)
-    //std::map<datatype, float> py;							// sam as px but py
-    //uint32_t countxy = y_true.size();						// countxy is the size of the input vectors
-
-
     /**
      if uncomment this line will parallelise the contingency matrix calucation
-    be carful to change pxy to pxy[0] and px to px[0] and py to py[0] in the rest of the code
+    be careful to change pxy to pxy[0] and px to px[0] and py to py[0] in the rest of the code
     **/
     std::map<std::pair<datatype, datatype>, float> pxy[16];
     std::map<datatype, float> px[16];
@@ -114,6 +109,93 @@ float Hxy(std::vector<datatype> &y_true, std::vector<datatype> &y_pred)
 }
 
 
+float norm_Hxy(std::vector<datatype> &y_true, std::vector<datatype> &y_pred)
+{
+	std::map<std::pair<datatype, datatype>, float> pxy[16];	// define map pxy that associate a pair of datatype with a float (joint probability)
+	std::map<datatype, float> px[16];	// define map px that associate a datatype with a float (marginal probability)
+	std::map<datatype, float> py[16];	// define map py that associate a datatype with a float (marginal probability)
+	uint32_t countxy = y_true.size();	// number of elements in the vectors y_true and y_pred
+
+    #pragma omp parallel												// start parallel region: here the computation of joint probability pxy, marginal probability px and py is parallelized
+    {
+        uint16_t nthreads = omp_get_num_threads();						// get the number of threads
+        uint16_t chunk_size = countxy / (nthreads + 1);					// define the chunk size
+        uint8_t thread_id = omp_get_thread_num();						// get the thread id
+        size_t start = thread_id * chunk_size;							// define the start index of the chunk
+        size_t end = (thread_id + 1) * chunk_size;						// define the end index of the chunk
+        for (size_t i = start; i < end; i++) {							// iterate over the chunk
+            pxy[thread_id][{y_true[i], y_pred[i]}] += 1.0 / countxy;	// increment the joint probability pxy
+            px[thread_id][y_true[i]] += 1.0 / countxy;					// increment the marginal probability px
+            py[thread_id][y_pred[i]] += 1.0 / countxy;					// increment the marginal probability py
+        }
+    }
+
+    #pragma omp master													// only the master thread executes this block
+    {
+        uint16_t nthreads = omp_get_max_threads();						// get the maximum number of threads
+        uint16_t chunk_size = countxy / (nthreads + 1);					// define the chunk size
+
+        for (size_t i = chunk_size * nthreads; i < countxy; i++) {		// iterate over the remaining elements
+            pxy[nthreads + 1][{y_true[i], y_pred[i]}] += 1.0 / countxy;	// increment the joint probability pxy
+            px[nthreads + 1][y_true[i]] += 1.0 / countxy;				// increment the marginal probability px
+            py[nthreads + 1][y_pred[i]] += 1.0 / countxy;				// increment the marginal probability py
+        }
+
+        for (int i = 1; i < nthreads + 2; i++) {
+            std::for_each(pxy[i].begin(), pxy[i].end(), [&](auto &p) {
+                pxy[0][p.first] += p.second;
+            });
+
+            std::for_each(px[i].begin(), px[i].end(), [&](auto &p) {
+                px[0][p.first] += p.second;
+            });
+
+            std::for_each(py[i].begin(), py[i].end(), [&](auto &p) {
+                py[0][p.first] += p.second;
+            });
+        }
+    }
+
+    float MI = 0.0;
+    #pragma omp parallel for reduction(+:MI)
+    for (size_t i = 0; i < pxy[0].size(); i++) {
+        auto pxx_iterator = std::next(pxy[0].begin(), i);
+        datatype x = pxx_iterator->first.first;
+        datatype y = pxx_iterator->first.second;
+        float p_joint = pxy[0][{x, y}];
+        float p_x = px[0][x];
+        float p_y = py[0][y];
+
+        if (p_joint > 0) {
+            MI += p_joint * log(p_joint / (p_x * p_y));
+        }
+    }
+
+    float Hx = 0.0, Hy = 0.0;
+    #pragma omp parallel for reduction(+:Hx)
+    for (size_t i = 0; i < px[0].size(); i++) {
+        auto px_iterator = std::next(px[0].begin(), i);
+        float p_x = px_iterator->second;
+        if (p_x > 0) {
+            Hx -= p_x * log(p_x);
+        }
+    }
+
+    #pragma omp parallel for reduction(+:Hy)
+    for (size_t i = 0; i < py[0].size(); i++) {
+        auto py_iterator = std::next(py[0].begin(), i);
+        float p_y = py_iterator->second;
+        if (p_y > 0) {
+            Hy -= p_y * log(p_y);
+        }
+    }
+
+    return (Hx + Hy > 0) ? 2 * MI / (Hx + Hy) : 0.0;
+}
+
+
+
+
 
 
 int main(int argc, char const *argv[])
@@ -176,7 +258,6 @@ int main(int argc, char const *argv[])
 	std::chrono::duration<double> duration = end - start;
 
 	// Write the MI matrix CSV file
-
     filename = filename.substr(0, filename.find_last_of(".")) + "_MI.csv";
 
 	std::ofstream outfile(filename);
