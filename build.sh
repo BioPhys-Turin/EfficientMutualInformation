a98abd3fdeb06e8363d911baea99888937ae34a3
Filippo Valle <filippo.valle@unito.it>
2025-04-04 15:52:23 +0200

    update build script

--- build.sh
+++ build.sh
@@ -1,10 +1,18 @@
+#!/bin/bash
+set -e
+export CXXFLAGS="-std=c++14 -O3 -Wall -Wextra -Wpedantic -Werror"
+
 rm -rf build
 mkdir -p build
 cd build
-export CXXFLAGS="-std=c++14 -O3 -Wall -Wextra -Wpedantic -Werror"
-cmake ..
+
+# Use the environment CXXFLAGS instead of hardcoding them here
+# This allows conda-build to set appropriate flags
+# export CXXFLAGS="-std=c++14 -O3 -Wall -Wextra -Wpedantic -Werror"
+
+cmake ${CMAKE_ARGS} ..
 make
-mv local/src/hk .
-mkdir -p $PREFIX/bin
-cp hk $PREFIX/bin/
-cp hk /usr/local/bin/
+
+# Create the bin directory and install the executable
+mkdir -p ${PREFIX}/bin
+cp local/src/hk ${PREFIX}/bin/
